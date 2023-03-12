328a6995104df7f8543bb5ed713c7f450088a6f9
Amaya Reed <amreed1@s.sfusd.edu>
2023-03-12 10:12:55 -0700

    n

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,7 +25,7 @@ public class PlayerController : MonoBehaviour
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
 
-        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f,0);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other) 
